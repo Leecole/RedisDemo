cbd7c523f70e9e0fd1662426fb7c13f0204faa31
Leecole <1903502727@qq.com>
2018-09-04 09:55:30 +0800

    first commit

--- src/main/java/com/oracle/redis/RedisJava.java
+++ src/main/java/com/oracle/redis/RedisJava.java
@@ -22,12 +22,14 @@ public class RedisJava {
 
         //查看服务是否运行
         System.out.println("服务正在运行："+jedis.ping());
+        System.out.println("*****************************************************");
 
 
 /********************************************2、字符串（String） 设置 redis 字符串数据******************************/
         jedis.set("runoobkey","Leecole");    //set user "leecole"
         //获取存储的数据以及输出
         System.out.println("redis 存储的字符串为："+jedis.get("runoobkey"));
+        System.out.println("*****************************************************");
 
 
 /********************************************3、列表 设置 redis 列表数据*******************************************/
@@ -37,15 +39,15 @@ public class RedisJava {
         jedis.lpush("dblist","mysql");
         jedis.lpush("dblist","sqlserver");
         //获取存取的数据并输出
-        jedis.lrange("dblist",0,10);
         //遍历
-        List<String> list=jedis.lrange("dblist",0,10);  //集合list的命令行输出：lrange(key,start,end)-----lrange("dblist",0,10)
+        List<String> list=jedis.lrange("dblist",0,3);  //集合list的命令行输出：lrange(key,start,end)-----lrange("dblist",0,10)
         for(int i=0;i<list.size();i++){
             System.out.println("列表项为："+list.get(i));
         }
+        System.out.println("*****************************************************");
 
  /********************************************3、keys 设置 redis keys实例*******************************************/
-        //获取数据并输出
+        //获取数据并输出所有的key
         Set<String> keys=jedis.keys("*");
         Iterator<String> it=keys.iterator();
         while(it.hasNext()){
@@ -53,6 +55,4 @@ public class RedisJava {
             System.out.println(key);
         }
     }
-
-
 }
